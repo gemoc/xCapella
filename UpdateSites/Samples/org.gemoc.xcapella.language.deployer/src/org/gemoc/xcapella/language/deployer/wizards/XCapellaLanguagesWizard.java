1c31308c8a0260b49248a711f6a51d62ebe5f255
smadelenat <sebastien.madelenat@gmail.com>
2016-03-18 10:11:27 +0100

    updating diagram representation and animation diagrams extensions. adding the last build.

--- UpdateSites/Samples/org.gemoc.xcapella.language.deployer/src/org/gemoc/xcapella/language/deployer/wizards/XCapellaLanguagesWizard.java
+++ UpdateSites/Samples/org.gemoc.xcapella.language.deployer/src/org/gemoc/xcapella/language/deployer/wizards/XCapellaLanguagesWizard.java
@@ -7,23 +7,33 @@ import java.util.List;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.gemoc.xcapella.language.deployer.Activator;
+import org.gemoc.xcapella.language.deployer.wizards.AbstractExampleWizard.ProjectDescriptor;
 
 
-public class ModeAutomatonWizard
+public class XCapellaLanguagesWizard
 	extends AbstractExampleWizard {
 	
 	protected Collection<ProjectDescriptor> getProjectDescriptors() {		
 		// We need the statements example to be unzipped along with the
 		// EMF library example model, edit and editor examples
 		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.xcapella.dataflow.animator.zip", "org.gemoc.xcapella.dataflow.animator"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.xcapella.dataflow.dse.zip", "org.gemoc.xcapella.dataflow.dse"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.xcapella.dataflow.k3dsa.zip", "org.gemoc.xcapella.dataflow.k3dsa"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.xcapella.dataflow.mocc.zip", "org.gemoc.xcapella.dataflow.mocc"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.xcapella.dataflow.xdsml.zip", "org.gemoc.xcapella.dataflow.xdsml"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/fr.inria.aoste.dataflowmode.constraint.moccml.zip", "fr.inria.aoste.dataflowmode.constraint.moccml"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.executionframework.engine.coordinator.commons.zip", "org.gemoc.executionframework.engine.coordinator.commons"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.gemoc_language_workbench.utils.zip", "org.gemoc.gemoc_language_workbench.utils"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/com.thalesgroup.trt.gemoc.modeautomata.animation.zip", "com.thalesgroup.trt.gemoc.modeautomata.animation"));
-//		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/com.thalesgroup.trt.gemoc.modeautomata.animation.zip", "./"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/com.thalesgroup.trt.mocc.modemachine.dse.zip", "com.thalesgroup.trt.mocc.modemachine.dse"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/com.thalesgroup.trt.mocc.modemachine.k3dsa.zip", "com.thalesgroup.trt.mocc.modemachine.k3dsa"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/com.thalesgroup.trt.mocc.modemachine.xdsml.zip", "com.thalesgroup.trt.mocc.modemachine.xdsml"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/fr.inria.aoste.timesquare.ccslkernel.solver.extension.statemachine.zip", "fr.inria.aoste.timesquare.ccslkernel.solver.extension.statemachine"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.smcube.moc.lib.zip", "org.gemoc.sample.smcube.moc.lib"));
 
+					
+		
 		return projects;
 	}
 
--- UpdateSites/Samples/org.gemoc.xcapella.model.deployer/src/org/gemoc/xcapella/model/deployer/wizards/XCapellaModelsWizard.java
+++ UpdateSites/Samples/org.gemoc.xcapella.model.deployer/src/org/gemoc/xcapella/model/deployer/wizards/XCapellaModelsWizard.java
@@ -6,15 +6,18 @@ import java.util.List;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.gemoc.xcapella.model.deployer.Activator;
+import org.gemoc.xcapella.model.deployer.wizards.AbstractExampleWizard.ProjectDescriptor;
 
 
-public class DataFlowWizard
+public class XCapellaModelsWizard
 	extends AbstractExampleWizard {
 	
 	protected Collection<ProjectDescriptor> getProjectDescriptors() {		
 
 		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/Dataflow.zip", "Dataflow"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/ModeAutomata.zip", "ModeAutomata"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/Coordination.zip", "Coordination"));
 		return projects;
 	}
 
