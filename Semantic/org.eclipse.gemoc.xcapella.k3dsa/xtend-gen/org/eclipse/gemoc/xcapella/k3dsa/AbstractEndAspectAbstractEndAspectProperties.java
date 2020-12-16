aee2631a3949d6164c81cdd3d43caf7c0f6dccdd
Dragonnier Tac <dragonnier-tac@inria.fr>
2020-12-16 11:34:00 +0100

    removes xtend-gen from git

--- Semantic/org.eclipse.gemoc.xcapella.k3dsa/xtend-gen/org/eclipse/gemoc/xcapella/k3dsa/AbstractEndAspectAbstractEndAspectProperties.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.gemoc.xcapella.k3dsa;
-
-@SuppressWarnings("all")
-public class AbstractEndAspectAbstractEndAspectProperties {
-  public Integer occurrenceNumber = Integer.valueOf(0);
-  
-  public Boolean occurred = Boolean.valueOf(false);
-}
--- Semantic/org.eclipse.gemoc.xcapella.k3dsa/xtend-gen/org/eclipse/gemoc/xcapella/k3dsa/ComponentExchangeAspectComponentExchangeAspectProperties.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.gemoc.xcapella.k3dsa;
-
-@SuppressWarnings("all")
-public class ComponentExchangeAspectComponentExchangeAspectProperties {
-  public Double value = Double.valueOf(0.0);
-}
